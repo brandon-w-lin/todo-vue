30ade4eea2006a2a9cafd28cc5172dc176dc5bd5
Brandon Lin <bwlin93@gmail.com>
2022-08-04 12:47:42 -0500

    adds routes for signup, login, todo pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import LoginView from "../views/LoginView.vue";
+import SignupView from "../views/SignupView.vue";
 import TodosIndex from "../views/TodosIndex.vue";
 
 const routes = [
@@ -13,6 +14,11 @@ const routes = [
     path: "/login",
     name: "login",
     component: LoginView,
+  },
+  {
+    path: "/signup",
+    name: "signup",
+    component: SignupView,
   },
   {
     path: "/todos",
