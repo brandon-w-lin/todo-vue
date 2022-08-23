e4bd38dabe6402cd535ffee1ee809655eff3b5b7
Brandon Lin <bwlin93@gmail.com>
2022-08-22 23:32:23 -0500

    adds nested draggable component

---
+++ src/components/nestedDraggable.vue
@@ -0,0 +1,37 @@
+<template>
+  <draggable
+    class="dragArea"
+    tag="ul"
+    :list="tasks"
+    :group="{ name: 'g1' }"
+    item-key="description"
+  >
+    <template #item="{ element }">
+      <li>
+        <p>{{ element.description }}</p>
+        <nested-draggable :tasks="element.todos" />
+      </li>
+    </template>
+  </draggable>
+</template>
+<script>
+import draggable from "vuedraggable";
+export default {
+  props: {
+    tasks: {
+      required: true,
+      type: Array,
+    },
+  },
+  components: {
+    draggable,
+  },
+  name: "nested-draggable",
+};
+</script>
+<style scoped>
+.dragArea {
+  min-height: 50px;
+  outline: 1px dashed;
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>Fun stuff about this page:</div>
+    <ul>
+      <li>
+        Updates are held in a queue on the front end and sync with the back end
+        whenever the user is inactive, or whenever the page is unloaded
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  data() {
+    return {
+      // data goes here
+    };
+  },
+};
+</script>
