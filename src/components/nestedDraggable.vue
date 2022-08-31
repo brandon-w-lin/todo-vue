1517066fa789165c149faaa877b66fd1b91834e4
Brandon Lin <bwlin93@gmail.com>
2022-08-30 19:14:55 -0500

    formatting for draggable list

--- src/components/nestedDraggable.vue
+++ src/components/nestedDraggable.vue
@@ -1,20 +1,26 @@
 <template>
   <draggable
-    class="dragArea"
+    class="item-container"
     tag="ul"
     :list="todos"
     :group="{ name: 'g1' }"
     item-key="description"
     @end="$emit('movedItem')"
+    ghost-class="ghost"
   >
     <template #item="{ element }">
-      <li>
-        <p>{{ element.description }}</p>
-        <nested-draggable :todos="element.todos" @movedItem="movedItem" />
-      </li>
+      <div>
+        <p class="item">{{ element.description }}</p>
+        <nested-draggable
+          class="item-sub"
+          :todos="element.todos"
+          @movedItem="movedItem"
+        />
+      </div>
     </template>
   </draggable>
 </template>
+
 <script>
 import draggable from "vuedraggable";
 export default {
@@ -34,11 +40,40 @@ export default {
       this.$emit("movedItem");
     },
   },
+  computed: {
+    dragOptions() {
+      return {
+        animation: 0,
+        group: "description",
+        disabled: false,
+        ghostClass: "ghost",
+      };
+    },
+  },
 };
 </script>
 <style scoped>
 .dragArea {
   min-height: 50px;
-  outline: 1px dashed;
+  outline: 1px solid;
+}
+
+.item-container {
+  /* max-width: 20rem; */
+  margin: 0;
+}
+.item {
+  padding: 1rem;
+  margin: 0.1rem;
+  border: solid black 1px;
+  background-color: #fefefe;
+}
+.item-sub {
+  margin: 0 0 0 1rem;
+}
+
+.ghost {
+  opacity: 0.5;
+  background: grey;
 }
 </style>
