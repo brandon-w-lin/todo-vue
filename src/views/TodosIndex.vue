66779d197819ff7161c014ca79e41b75af127162
Brandon Lin <bwlin93@gmail.com>
2022-08-31 21:19:14 -0500

    removes check boxes from un-nested todo list

--- src/views/TodosIndex.vue
+++ src/views/TodosIndex.vue
@@ -21,20 +21,6 @@
     >
       <template #item="{ element }">
         <div class="list-group-item grabbable">
-          <span class="completed-box">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              viewBox="0 0 16 16"
-              :class="completed(element)"
-              @click="setComplete(element)"
-            >
-              <path class="circle" />
-              <path class="check" />
-            </svg>
-          </span>
           <span
             class="description"
             contenteditable="true"
@@ -240,26 +226,6 @@ export default {
       // todo.updateDescription = true;
     },
 
-    // COMPLETED
-    setComplete(todo) {
-      todo.completed = todo.completed == 0 ? 1 : 0;
-      // this.updateTodo(todo);
-      this.needToSyncWithServer = true;
-    },
-    completed(todo) {
-      switch (todo.completed) {
-        case 0:
-          // console.log("incomplete");
-          return "unchecked";
-        case 1:
-          // console.log("completed");
-          return "checked";
-        case 2:
-          // console.log("moved to completed section");
-          return "checked";
-      }
-    },
-
     // SINGLE REST ACTIONS (DEPRECATED -> USE BATCH)
     getTodos() {
       axios.get("http://localhost:3000/todos", this.config).then((response) => {
@@ -320,28 +286,6 @@ export default {
 
 [contenteditable] {
   outline: 0px solid transparent;
-}
-
-.completed-box {
-  cursor: pointer;
-  margin: 5px;
-}
-.unchecked .circle {
-  d: path(
-    "M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
-  );
-}
-
-.unchecked:hover .check {
-  d: path(
-    "M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"
-  );
-}
-
-.checked .circle {
-  d: path(
-    "M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"
-  );
 }
 
 .grabbable {
